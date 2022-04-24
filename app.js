cf6b4b2ff48867f316b6506f00c503bedae61c86
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-04-24 14:13:36 -0500

    V 0.0.11

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 //vargables!
 
-//main game counters
-// let clickButton = document.getElementById('clickButton').addEventListener('click', patGoodBoy)
-
 let pats = 0
 let patsPerSecond = 0
 let arms = 1
@@ -10,23 +7,100 @@ let arms = 1
 //"inventory"
 let roundBoys = 0
 let longBoys = 0
-let gonvahs = 0
+let govnahs = 0
 
 // item shop vargables
 let roundBoyCost = 25
-let longBoyCost = 100
-let govnahCost = 250
-let fineCost = 1001
+let longBoyCost = 450
+let govnahCost = 200
+let fineCost = 9001
 
 //FUNCTIONS?!?!?
+//main game counters and HTML selection
+const lab = document.getElementById('lab')
+const patsNumber = document.getElementById('patsNumber')
+const pps = document.getElementById('patsPerSecond')
+const patsPerClick = document.getElementById('patsPerClick')
 
-function patGoodBoy() {
+lab.addEventListener('click', function() {
     pats += arms;
-    document.getElementById('patsNumber').innerText = pats;
+    patsNumber.innerText = pats
+    // console.log(pats)
+})
+
+// pats per second timer
+setInterval(function() {
     
+        pats += patsPerSecond;
 
-}
+        patsNumber.innerText = pats
+}, 1000)
 
-console.log(pats)
+// round boys
+const roundBoyTotal = document.getElementById('roundBoyTotal')
+const roundBoyCostAmt = document.getElementById('roundBoyCost')
+
+const roundBoyBuyBtn = document.getElementById('roundBoyBuyBtn').addEventListener('click', function() {
+    if (pats >= roundBoyCost) {
+        roundBoys++
+        patsPerSecond++
+        roundBoyCost = Math.floor(roundBoyCost * 1.7)
+        roundBoyCostAmt.innerText = roundBoyCost
+        roundBoyTotal.innerText = roundBoys
+        pps.innerText = patsPerSecond
+
+    } else {
+        alert('You do not have enough pats!')
+    }
+})
+
+// long boys
+const longBoyTotal = document.getElementById('longBoyTotal')
+const longBoyCostAmt = document.getElementById('longBoyCost')
+
+const longBoyBuyBtn = document.getElementById('longBoyBuyBtn').addEventListener('click', function() {
+    if (pats >= longBoyCost) {
+        longBoys++
+        patsPerSecond = patsPerSecond + 5
+        longBoyCost = Math.floor(longBoyCost * 2.2)
+        longBoyCostAmt.innerText = longBoyCost
+        longBoyTotal.innerText = longBoys
+        pps.innerText = patsPerSecond
+
+    } else {
+        alert('You do not have enough pats!')
+    }
+})
+
+//Govnahs!
+const govnahTotal = document.getElementById('govnahTotal')
+const govnahCostAmt = document.getElementById('govnahCost')
+
+const govnahBuyBtn = document.getElementById('govnahBuyBtn').addEventListener('click', function() {
+    if (pats >= govnahCost) {
+        govnahs++
+        arms++
+        govnahCost = Math.floor(govnahCost * 1.5)
+        govnahCostAmt.innerText = govnahCost
+        govnahTotal.innerText = govnahs
+        patsPerClick.innerText = arms
+    } else {
+        alert('You do not have enough pats!')
+    }
+})
 
 
+const fineCostAmt = document.getElementById('fineCost')
+
+const fineBuyBtn = document.getElementById('fineBuyBtn').addEventListener('click', function() {
+    if (pats >= fineCost) {
+    
+        patsPerSecond = patsPerSecond + 100
+        fineCostAmt.innerText = `Already Adopted!`
+        pps.innerText = patsPerSecond
+        fineBuyBtn.removeEventListener('click')
+
+    } else {
+        alert('You do not have enough pats!')
+    }
+})
